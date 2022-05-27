43709e34a1d8b5264aeebc81b62d12ebd6162c94
Yamila Levalle <38963831+ylevalle@users.noreply.github.com>
2022-05-27 16:52:24 -0300

    Add files via upload

---
+++ InsecureEtherVault.sol
@@ -0,0 +1,30 @@
+//Source: https://gist.github.com/serial-coder/5a29d95e1872c960950d4a00b36768e6#file-insecureethervault-sol
+
+pragma solidity 0.8.13;
+
+contract InsecureEtherVault {
+    mapping (address => uint256) private userBalances;
+
+    function deposit() external payable {
+        userBalances[msg.sender] += msg.value;
+    }
+
+    function withdrawAll() external {
+        uint256 balance = getUserBalance(msg.sender);
+        require(balance > 0, "Insufficient balance");
+
+        (bool success, ) = msg.sender.call{value: balance}("");
+        require(success, "Failed to send Ether");
+
+        userBalances[msg.sender] = 0;
+    }
+
+    function getBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function getUserBalance(address _user) public view returns (uint256) {
+        return userBalances[_user];
+    }
+}
+
